05b1432bced6f0cddb2336a16cb5dd3955c6ee18
Gage Pacier <lead.diet@gmail.com>
2021-05-19 10:44:20 -0400

    broke out parsing arguements

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     //println!("{:?}", args); debug line
 
-    let query = &args[1];
-    let filename =&args[2];
+    let (query, filename) = parse_configs(&args);
 
     println!("Searching for {} in file: {}", query, filename); //debug line
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_configs(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename =&args[2];
+
+    (query, filename)
 }
