69fb06f1685d8ca8ac44671f3c2e1ede304c2cc4
Gage Pacier <lead.diet@gmail.com>
2021-05-19 11:11:07 -0400

    Broke out run, better error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,21 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //println!("{:?}", args); debug line
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| { println!("Problem parsing arguments: {}", err); process::exit(1)});
 
     println!("Searching for {} in file: {}", config.query, config.filename); //debug line
 
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -20,10 +24,21 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
