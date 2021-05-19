378c6f51404a701951561bca61e0d33333eb93c8
Gage Pacier <lead.diet@gmail.com>
2021-05-19 10:41:14 -0400

    basic read and sample poem loaded

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    //println!("{:?}", args); debug line
+
+    let query = &args[1];
+    let filename =&args[2];
+
+    println!("Searching for {} in file: {}", query, filename); //debug line
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
