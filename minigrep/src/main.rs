553dcc146bf216e9708df8354816bfaa0a12dcf3
ag-pacier <lead.diet@gmail.com>
2021-05-21 21:11:12 -0400

    switched to iter for Config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     //println!("{:?}", args); debug line
 
-    let config = Config::new(&args).unwrap_or_else(|err| { eprintln!("Problem parsing arguments: {}", err); process::exit(1)});
+    let config = Config::new(env::args()).unwrap_or_else(|err| { eprintln!("Problem parsing arguments: {}", err); process::exit(1)});
 
     println!("Searching for {} in file: {}", config.query, config.filename); //debug line
 
